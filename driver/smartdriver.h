adfa6160e9ab4b6abbebb19cbdc126f8c5de6193
virtualor3 <2421268995@qq.com>
2022-08-09 09:31:47 +0800

    interface

--- driver/smartdriver.h
+++ driver/smartdriver.h
@@ -3,7 +3,13 @@
 #include <stdint.h>
 
 //温湿度读取
-// return - uint32_t: 
+// return - uint32_t: 温度低16位、湿度高16位
 extern uint32_t read_temperature_humidity(void);
+//从read_temperature_humidity返回值获取温度
+#define GET_TEMP(x) (x & 0xffff)    //可重定义
+//从read_temperature_humidity返回值获取湿度
+#define GET_HUM(x)  (x >> 16)       //可重定义
+//温度读取
 extern uint32_t read_temperature(void);
+//湿度读取
 extern uint32_t read_humidity(void);
